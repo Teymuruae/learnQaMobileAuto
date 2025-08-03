621de858c92e4823fb3f3ac9de53fdd10b5dfa2a
teymuruae <baratov.teymur@yandex.ru>
2025-08-03 14:21:23 +0300

    ex7 homework done

--- src/test/java/tests/ChangeAppConditionTest.java
+++ src/test/java/tests/ChangeAppConditionTest.java
@@ -7,6 +7,7 @@ import lib.ui.SearchPageObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
 
 public class ChangeAppConditionTest extends CoreTestCase {
 
@@ -17,40 +18,47 @@ public class ChangeAppConditionTest extends CoreTestCase {
     @Test
     void changeScreenOrientationOnSearchResultTest() {
         final String articleTitle = "Java (programming language)";
-        mainPageObject.skip();
-        searchPageObject
-                .initSearchInput()
-                .typeSearchLine("Java")
-                .clickArticle("Object-oriented programming language")
-                .waitForTitleElement(articleTitle);
+       try {
+           mainPageObject.skip();
+           searchPageObject
+                   .initSearchInput()
+                   .typeSearchLine("Java")
+                   .clickArticle("Object-oriented programming language")
+                   .waitForTitleElement(articleTitle);
 
-        String titleBeforeRotation = mainPageObject.waitForElementAndGetAttributeValue(
-                By.xpath("//android.view.View[@content-desc='Java (programming language)']"),
-                "name",
-                "Cant find topic title"
-        );
+           String titleBeforeRotation = mainPageObject.waitForElementAndGetAttributeValue(
+                   By.xpath("//android.view.View[@content-desc='Java (programming language)']"),
+                   "name",
+                   "Cant find topic title"
+           );
 
-        rotateScreenLandscape();
+           rotateScreenLandscape();
 
-        String titleAfterRotation = mainPageObject.waitForElementAndGetAttributeValue(
-                By.xpath("//android.view.View[@content-desc='Java (programming language)']"),
-                "name",
-                "Cant find topic title"
-        );
+           String titleAfterRotation = mainPageObject.waitForElementAndGetAttributeValue(
+                   By.xpath("//android.view.View[@content-desc='Java (programming language)']"),
+                   "name",
+                   "Cant find topic title"
+           );
 
-        Assertions.assertEquals(titleBeforeRotation, titleAfterRotation,
-                "Titles after rotation are different. Before: %s, After: %s".formatted(titleBeforeRotation, titleAfterRotation));
+           Assertions.assertEquals(titleBeforeRotation, titleAfterRotation,
+                   "Titles after rotation are different. Before: %s, After: %s".formatted(titleBeforeRotation, titleAfterRotation));
 
-        rotateScreenPortrait();
+           rotateScreenPortrait();
 
-        String titleAfterSecondRotation = mainPageObject.waitForElementAndGetAttributeValue(
-                By.xpath("//android.view.View[@content-desc='Java (programming language)']"),
-                "name",
-                "Cant find topic title"
-        );
+           String titleAfterSecondRotation = mainPageObject.waitForElementAndGetAttributeValue(
+                   By.xpath("//android.view.View[@content-desc='Java (programming language)']"),
+                   "name",
+                   "Cant find topic title"
+           );
 
-        Assertions.assertEquals(titleAfterRotation, titleAfterSecondRotation,
-                "Titles after rotation are different. Before: %s, After: %s".formatted(titleBeforeRotation, titleAfterRotation));
+           Assertions.assertEquals(titleAfterRotation, titleAfterSecondRotation,
+                   "Titles after rotation are different. Before: %s, After: %s".formatted(titleBeforeRotation, titleAfterRotation));
+       }finally {
+           final ScreenOrientation orientation = driver.getOrientation();
+           if (orientation.value().equals(ScreenOrientation.LANDSCAPE.value())){
+               rotateScreenPortrait();
+           }
+       }
     }
 
     @Test
